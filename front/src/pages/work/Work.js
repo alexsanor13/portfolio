8dc6c1183105c3f0a5787e8b274ef03d56cc74d2
Alexso47 <alekox97@gmail.com>
2022-05-24 18:47:17 +0200

    Projects updated.

--- front/src/pages/work/Work.js
+++ front/src/pages/work/Work.js
@@ -1,26 +1,87 @@
 import './Work.css';
 
-import projectPic from '../../assets/blog.jpg'
-import processPDF from '../../assets/docs/dfp_process.pdf'
-import paperPDF from '../../assets/docs/dfp_paper.pdf'
+import tfgPic from '../../assets/tfg/tfg.jpeg'
+import processPDF from '../../assets/tfg/dfp_process.pdf'
+import paperPDF from '../../assets/tfg/dfp_paper.pdf'
 
-import htmlLogo from '../../assets/icons/html.png'
-import cssLogo from '../../assets/icons/css.png'
-import jsLogo from '../../assets/icons/javascript.png'
-import jqueryLogo from '../../assets/icons/jquery.png'
-import bootstrapLogo from '../../assets/icons/bootsrap.png'
-import csharpLogo from '../../assets/icons/csharp.png'
-import sqlServerLogo from '../../assets/icons/sqlserver.png'
-import azureLogo from '../../assets/icons/azure.png'
-import logicAppsLogo from '../../assets/icons/logicapps.png'
+import htmlLogo from '../../assets/technologies/html.png'
+import cssLogo from '../../assets/technologies/css.png'
+import jsLogo from '../../assets/technologies/javascript.png'
+import jqueryLogo from '../../assets/technologies/jquery.png'
+import bootstrapLogo from '../../assets/technologies/bootsrap.png'
+import csharpLogo from '../../assets/technologies/csharp.png'
+import sqlServerLogo from '../../assets/technologies/sqlserver.png'
+import azureLogo from '../../assets/technologies/azure.png'
+import logicAppsLogo from '../../assets/technologies/logicapps.png'
+import reactLogo from '../../assets/technologies/react.png'
+import nodeLogo from '../../assets/technologies/node.png'
+import mongodbLogo from '../../assets/technologies/mongodb.png'
+
+import notesVideo from '../../assets/notes-app/notes-app.mp4'
+import countriesVideo from '../../assets/countries-app/countries-app.mp4'
+
 
 
 export const Work = () => {
+
+    const toggleFullScreen = (video) => {
+        let el = document.getElementById("fullscreen-"+video+"Video");
+        if (el.requestFullscreen) {
+          el.requestFullscreen();
+        } else if (el.msRequestFullscreen) {
+          el.msRequestFullscreen();
+        } else if (el.mozRequestFullScreen) {
+          el.mozRequestFullScreen();
+        } else if (el.webkitRequestFullscreen) {
+          el.webkitRequestFullscreen();
+        }
+    };
+
   return (
     <main id="main-wrapper">
         <section id="work-content">
             <article className='project-item'>
-              <img src={projectPic} alt="Degree final project"/>
+                <video loop autoPlay muted id="fullscreen-countriesVideo"
+                    src={countriesVideo}
+                    type="video/mp4"
+                />
+                <div className='project-info'>
+                    <h3>Countries-app</h3>
+                    <p>
+                        You are able to search a country and some of its information as weather, population, etc.
+                    </p>
+                    <p>Implemented to learn Reactjs, fetching data from APIs, etc.</p>
+                    <div className="tecnologies">
+                        <span><img title='react' alt='react' src={reactLogo}/></span>
+                        <span><img title='node' alt='node' src={nodeLogo}/></span>
+                    </div>
+                    <br/>
+                    <button className='btn-two small' onClick={() => toggleFullScreen("countries")}>Fullscreen</button>
+                </div>
+            </article>
+            <article className='project-item'>
+                <video loop autoPlay muted controls id="fullscreen-notesVideo"
+                    src={notesVideo}
+                    type="video/mp4"
+                />
+                <div className='project-info'>
+                    <h3>Notes-app</h3>
+                    <p>
+                    Web application where you can annotate, with your own user, stuff that might need to remember later. 
+                    Notes can be edited, filtered, marked as important, among others.
+                    </p>
+                    <p>Implemented to learn Reactjs, Nodejs and MongoDB.</p>
+                    <div className="tecnologies">
+                        <span><img title='react' alt='react' src={reactLogo}/></span>
+                        <span><img title='node' alt='node' src={nodeLogo}/></span>
+                        <span><img title='mongodb' alt='mongodb' src={mongodbLogo}/></span>
+                    </div>
+                    <br/>
+                    <button className='btn-two small' onClick={() => toggleFullScreen("notes")}>Fullscreen</button>
+                </div>
+            </article>    
+            <article className='project-item'>
+              <img src={tfgPic} alt="Degree final project"/>
                 <div className='project-info'>
                     <h3>Degree Final Project</h3>
                     <p>
@@ -48,7 +109,7 @@ export const Work = () => {
                         <span><img title='logic apps' alt='logic apps' src={logicAppsLogo}/></span>
                     </div>
                 </div>
-            </article>        
+            </article>    
         </section>
     </main>
   );
